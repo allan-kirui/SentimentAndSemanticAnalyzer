bb1eab67a0e3c32ec5d08bd0f179dbca09cef85d
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2024-03-21 14:41:11 +0100

    Added Semantic visualisation

---
+++ SemanticAnalyzer.py
@@ -0,0 +1,48 @@
+from gensim import corpora, models
+import pandas as pd
+
+class SemanticAnalyzer:
+    def __init__(self):
+        self.dictionary = None
+        self.lda_model = None
+        self.corpus = None
+
+    def train_lda_model(self, processed_texts, num_topics=5):
+        # Create a dictionary from the processed texts
+        self.dictionary = corpora.Dictionary(processed_texts)
+        
+        # Convert tokenized documents into bag-of-words representation
+        self.corpus = [self.dictionary.doc2bow(text) for text in processed_texts]
+
+        # Train the LDA model
+        self.lda_model = models.LdaModel(self.corpus, num_topics=num_topics, id2word=self.dictionary, passes=10)
+
+    def get_topics(self):
+        # Get the topics from the LDA model
+        return self.lda_model.print_topics()
+    
+
+    def infer_topics(self, new_text):
+        # Preprocess the new text
+        processed_text = preprocess_text(new_text)
+
+        # Convert the processed text into bag-of-words representation
+        bow_vector = self.dictionary.doc2bow(processed_text)
+
+        # Infer the topic distribution for the new text
+        topic_distribution = self.lda_model[bow_vector]
+
+        return topic_distribution
+
+    def visualize_topics(self):
+        # Visualize the topics using pyLDAvis
+        import pyLDAvis.gensim
+        
+        # Prepare the visualization
+        #pyLDAvis.enable_notebook()
+        lda_display = pyLDAvis.gensim.prepare(self.lda_model, self.corpus, self.dictionary)
+        
+        # save the visualization
+        pyLDAvis.save_html(lda_display, 'topics/visualisation.html')
+
+    # Additional methods for inference or topic visualization can be added here
