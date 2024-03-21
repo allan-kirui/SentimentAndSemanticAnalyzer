150619725564fbcdbfece94ecf689cd8dbfe889b
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2024-03-21 14:36:38 +0100

    Updated the semantic analyzer

--- SentimentAnalyzer.py
+++
@@ -1,9 +0,0 @@
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-class SentimentAnalyzer:
-    def __init__(self):
-        self.analyzer = SentimentIntensityAnalyzer()
-
-    def perform_sentiment_analysis(self, text):
-        sentiment_score = self.analyzer.polarity_scores(text)
-        return sentiment_score['compound']
